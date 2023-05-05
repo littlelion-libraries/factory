505571b257e04f738b69ea8e719d24734775fce5
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-05-05 18:09:09 +0700

    1.3.0

--- UnityHashFactory.cs
+++ UnityHashFactory.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Object = UnityEngine.Object;
 
 namespace Factories
@@ -7,6 +8,11 @@ namespace Factories
     public class UnityHashFactory<T> : IHashFactory<T>
     {
         private Dictionary<string, Object> _objects = new();
+
+        public IEnumerable<Object> ObjectArray
+        {
+            set => _objects = value.ToDictionary(it => it.name);
+        }
 
         public Dictionary<string, Object> Objects
         {
