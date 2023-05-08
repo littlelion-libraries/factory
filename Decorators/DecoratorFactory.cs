a524feec42cec2ac858851c3d92f523ff9a9164b
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-05-08 09:05:37 +0700

    1.9.1

--- Decorators/DecoratorFactory.cs
+++ Decorators/DecoratorFactory.cs
@@ -2,13 +2,13 @@ namespace Factories.Decorators
 {
     public class DecoratorFactory<TMaterial, TProduct> : IFactory<TMaterial, TProduct>
     {
-        public IDecorator<TMaterial, TProduct> Decorator { get; set; }
+        public IDecorator<TProduct, TMaterial> Decorator { get; set; }
         public IFactory<TMaterial, TProduct> Impl { get; set; }
 
         public TProduct Create(TMaterial material)
         {
             var product = Impl.Create(material);
-            Decorator.Decorate(material, product);
+            Decorator.Decorate(product, material);
             return product;
         }
     }
