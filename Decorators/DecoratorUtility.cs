806ba39fd8e07d92842c1868cc36716db6f6ef27
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-05-09 16:59:21 +0700

    1.12.0

--- Decorators/DecoratorUtility.cs
+++ Decorators/DecoratorUtility.cs
@@ -4,18 +4,41 @@ namespace Factories.Decorators
 {
     public static class DecoratorUtility
     {
-        public static IDecorator<TDesObj, TDesParam> Select<TDesObj, TDesParam, TSrcObj, TSrcParam>(
-            this IDecorator<TSrcObj, TSrcParam> decorator,
-            Func<TDesObj, TSrcObj> objTrans,
-            Func<TDesParam, TSrcParam> paramTrans
+        public struct Decorator<TObject, TParameter>
+        {
+        }
+        
+        public static Decorator<TProduct, TMaterial> Create<TProduct, TMaterial>()
+        {
+            return new Decorator<TProduct, TMaterial>();
+        }
+        
+        public static TransformDecorator<TDesObj, TParam, TSrcObj, TParam> Select<TDesObj, TParam, TSrcObj>(
+            this Decorator<TDesObj, TParam> _,
+            IDecorator<TSrcObj, TParam> decorator,
+            Func<TDesObj, TSrcObj> objTrans
         )
         {
-            return new TransformDecorator<TDesObj, TDesParam, TSrcObj, TSrcParam>
+            return new TransformDecorator<TDesObj, TParam, TSrcObj, TParam>
             {
                 ObjTransform = objTrans,
-                ParamTransform = paramTrans,
-                Src = decorator
+                ParamTransform = param => param,
+                // Src = decorator
             };
         }
+        
+        // public static IDecorator<TDesObj, TDesParam> Select<TDesObj, TDesParam, TSrcObj, TSrcParam>(
+        //     this IDecorator<TSrcObj, TSrcParam> decorator,
+        //     Func<TDesObj, TSrcObj> objTrans,
+        //     Func<TDesParam, TSrcParam> paramTrans
+        // )
+        // {
+        //     return new TransformDecorator<TDesObj, TDesParam, TSrcObj, TSrcParam>
+        //     {
+        //         ObjTransform = objTrans,
+        //         ParamTransform = paramTrans,
+        //         Src = decorator
+        //     };
+        // }
     }
 }
--- FactoryUtility.cs
+++ FactoryUtility.cs
@@ -4,6 +4,28 @@ namespace Factories
 {
     public static class FactoryUtility
     {
+        public struct Factory<TProduct, TMaterial>
+        {
+        }
+
+        public static Factory<TProduct, TMaterial> Create<TProduct, TMaterial>()
+        {
+            return new Factory<TProduct, TMaterial>();
+        }
+
+        public static IFactory<TDesMaterial, TProduct> Select<TDesMaterial, TProduct, TSrcMaterial>(
+            this Factory<TDesMaterial, TProduct> _,
+            IFactory<TSrcMaterial, TProduct> factory,
+            Func<TDesMaterial, TSrcMaterial> transform
+        )
+        {
+            return new TransformFactory<TDesMaterial, TProduct, TSrcMaterial, TProduct>
+            {
+                Src = factory,
+                Transform = transform
+            };
+        }
+
         public static IFactory<TDesMaterial, TProduct> Select<TDesMaterial, TProduct, TSrcMaterial>(
             this IFactory<TSrcMaterial, TProduct> factory,
             Func<TDesMaterial, TSrcMaterial> transform
