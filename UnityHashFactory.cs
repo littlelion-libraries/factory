ead4f8c2ed3dd04ee6e5fec7aedb6d092b4b6026
hoangtuincepit <128338344+hoangtuincepit@users.noreply.github.com>
2023-05-11 08:20:54 +0700

    1.15.0

--- UnityHashFactory.cs
+++ UnityHashFactory.cs
@@ -1,27 +1,26 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using Object = UnityEngine.Object;
+using UnityEngine;
 
 namespace Factories
 {
-    public class UnityHashFactory<T> : IHashFactory<T>
+    public class UnityHashFactory<T> : IHashFactory<T> where T : Object
     {
-        private Dictionary<string, Object> _objects = new();
+        private Dictionary<string, T> _objects = new();
 
-        public IEnumerable<Object> ObjectArray
+        public IEnumerable<T> ObjectArray
         {
             set => _objects = value.ToDictionary(it => it.name);
         }
 
-        public Dictionary<string, Object> Objects
+        public Dictionary<string, T> Objects
         {
             set => _objects = value;
         }
 
         public T Create(string material)
         {
-            return Object.Instantiate(_objects[material]) is T t ? t : throw new InvalidCastException();
+            return Object.Instantiate(_objects[material]);
         }
     }
 }
